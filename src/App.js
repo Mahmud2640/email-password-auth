285670c77bacdafa3b55b2adb1dd1bf217e1358c
Mahmud2640 <mdmahmud484@gmail.com>
2022-04-09 16:04:15 +0600

    added validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import "./App.css";
 import app from "./firebase.init";
+import { useState } from "react";
 
 const auth = getAuth(app);
 
 function App() {
-  const handleEmailBlur = (e) =>{
-    console.log(e.target.value);
-  }
-  const handlePasswordBlur = (e) =>{
-    console.log(e.target.value);
-  }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState("");
 
+  const handleEmailBlur = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordBlur = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.stopPropagation();
+      return;
+    }
+    if(!/(?=.*[A-Z])/.test(password)){
+      setError("Password must contain at least one uppercase letter");
+      return;
+    }
+    setValidated(true);
+    setError("");
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    e.preventDefault();
+    console.log("submit", email, password);
+  };
 
   return (
-    <div className="form">
-      <h2 className="header">Login Form</h2>
-      <input onBlur={handleEmailBlur} type="email" name="email" placeholder="Enter Email Here" />
-      <input onBlur={handlePasswordBlur} type="password" name="" placeholder="Enter Password Here" />
-      <button className="btn">Login</button>
-      <button className="btn">Sing In</button>
+    <div>
+      <div className="submit w-50 mx-auto mt-5">
+        <h2 className="text-primary header">Please Register</h2>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              onBlur={handleEmailBlur}
+              type="email"
+              placeholder="Enter email"
+              required
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid Email.
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              onBlur={handlePasswordBlur}
+              type="password"
+              placeholder="Password"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid Password.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember Me" />
+          </Form.Group>
+          <p className="text-danger">{error}</p>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
